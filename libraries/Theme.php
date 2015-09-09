2ff1248bd4cd56678b39901120b2d607528ce3ef
Rogerio Taques <rogerio.taques@rakuten.com>
2015-09-09 12:39:25 +0900

    Revert "Added support for Template Parser Class."
    This reverts commit 4e3e25b898d31ba75528aaeb79400dea2f497c21.

--- libraries/Theme.php
+++ libraries/Theme.php
@@ -2,80 +2,65 @@
 
 /**
  * CI-Theme
- * Provides a simple way to implement a theme based website or app using Codeigniter.
+ * Provides a simple way to implement a theme based website or app using Codeigniter.  
  *
  * Licensed under MIT license:
  * http://www.opensource.org/licenses/mit-license.php
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
  * to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
  * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @requires PHP5+
  * @author Rogério Taques (rogerio.taques@gmail.com)
  * @see https://github.com/rogeriotaques/ci-theme
- * @version 1.3
- *
+ * @version 1.2
+ * 
  */
 
-class Theme
+class Theme 
 {
 		private $ci = NULL;
-
+		
 		private $var_name = 'outputs';
 		private $theme_data = array();
 		private $theme_path = '../themes/';
-
+		
 		private $assets = array();
 		private $metatags = array();	// array( array('param' => 'value') )
-
+		
 		function __construct()
 		{
 			$this->ci =& get_instance();
 			$this->ci->config->load('theme', FALSE, TRUE);
-
+			
 			if ( $this->ci->config->item('output_var_name') )
 			{
 				$this->var_name = $this->ci->config->item('output_var_name');
 			}
-
+			
 			if ( $this->ci->config->item('theme_path') )
 			{
 				$this->theme_path = $this->ci->config->item('theme_path');
-			}
-
+			} 
+			
 			$this->metatags = array(
-
-				// pre defined
-				'name-viewport' => array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no'),
-				'name-robots' => array('name' => 'robots', 'content' => 'index,follow,all'),
-				'http-equiv-content-type' => array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=utf-8'),
-				'http-equiv-content-style-type' => array('http-equiv' => 'Content-Style-Type', 'content' => 'text/css'),
-
-                // basic html tags
-				'name-charset' => array('charset' => 'utf-8'),
-				'name-title' => array('name' => 'title', 'content' => ''),
-				'name-description' => array('name' => 'description', 'content' => ''),
-				'name-keywords' => array('name' => 'keywords', 'content' => ''),
-				'name-author' => array('name' => 'author', 'content' => ''),
-				'name-publisher' => array('name' => 'publisher', 'content' => ''),
-				'name-generator' => array('name' => 'generator', 'content' => ''),
-
+                
                 // facebook api
                 'property-og-type' => array('property' => 'fb:app_id', 'content' => ''),
                 'property-og-type' => array('property' => 'fb:profile_id', 'content' => ''),
-
+			
 				// open-graph
 	        	'property-og-type' => array('property' => 'og:type', 'content' => 'website'),
 	        	'property-og-title' => array('property' => 'og:title', 'content' => ''),
@@ -83,10 +68,25 @@ class Theme
 	        	'property-og-image' => array('property' => 'og:image', 'content' => ''),
 	        	'property-og-site-name' => array('property' => 'og:site_name', 'content' => ''),
 	        	'property-og-url' => array('property' => 'og:url', 'content' => ''),
+				
+				// pre defined
+				'name-viewport' => array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no'),
+				'name-robots' => array('name' => 'robots', 'content' => 'index,follow,all'),
+				'http-equiv-content-type' => array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=utf-8'),
+				'http-equiv-content-style-type' => array('http-equiv' => 'Content-Style-Type', 'content' => 'text/css'),
+                
+                // basic html tags
+				'name-title' => array('name' => 'title', 'content' => ''),
+				'name-description' => array('name' => 'description', 'content' => ''),
+				'name-keywords' => array('name' => 'keywords', 'content' => ''),
+				'name-author' => array('name' => 'author', 'content' => ''),
+				'name-publisher' => array('name' => 'publisher', 'content' => ''),
+				'name-generator' => array('name' => 'generator', 'content' => ''),
+                
 			);
-
+			
 		}
-
+		
 		/**
 		 * Sets a new variable data to be passed for theme output
 		 * @param $name
@@ -97,7 +97,7 @@ class Theme
 		{
 			$this->theme_data[$name] = $value;
 		}
-
+	
 		/**
 		 * Loads a view with a given theme.
 		 * @param string 	$theme
@@ -111,24 +111,7 @@ class Theme
 			$this->set($this->var_name, $this->ci->load->view($view, $view_data, TRUE));
 			return $this->ci->load->view($this->theme_path . $theme, $this->theme_data, $return);
 		}
-
-		/**
-		 * Parses a template performing simple text substitution for pseudo-variables.
-		 * This method uses has depency on the parser library of Codeigniter.
-		 *
-		 * @param string 	$theme
-		 * @param string 	$view
-		 * @param array 	$view_data
-		 * @param boolean 	$return
-		 * @return void
-		 */
-		function parse($theme = '', $view = '' , $view_data = array(), $return = FALSE)
-		{
-			$this->ci->load->library('parser');
-			$this->set($this->var_name, $this->ci->parser->parse($view, $view_data, TRUE));
-			return $this->ci->load->view($this->theme_path . $theme, $this->theme_data, $return);
-		}
-
+				
 		/**
 		 * A private function to set assets for theme.
 		 * @return void
@@ -137,19 +120,19 @@ class Theme
 		{
 			$ci =& get_instance();
 			$assets =& $ci->theme->assets;
-
+			
 			if (!isset( $assets[$group] ))
 			{
 				$assets[$group] = array();
 			}
-
+			
 			if ( is_array($asset) )
 			{
 				foreach ($asset as $a)
 				{
 					if ( !in_array($a, $assets[$group]) )
 					{
-
+						
 						$assets[$group][] = $a;
 					}
 				}
@@ -158,9 +141,9 @@ class Theme
 			{
 				$assets[$group][] = $asset;
 			}
-
+			
 		} // set_asset
-
+		
 		/**
 		 * A private function to get assets for theme.
 		 * @return void
@@ -169,16 +152,16 @@ class Theme
 		{
 			$ci =& get_instance();
 			$assets = $ci->theme->assets;
-
+			
 			if ( !isset($assets[$group]) )
 			{
 				return FALSE;
 			}
-
+			
 			return count($assets[$group]) ? $assets[$group] : FALSE;
 
 		} // set_asset
-
+		
 		/**
 		 * Sets CSS files to be outputed into theme files.
 		 * @param variant $css - String or array(string)
@@ -188,7 +171,7 @@ class Theme
 		{
 			self::set_asset($css, 'css');
 		}
-
+		
 		/**
 		 * Sets JS files to be outputed into theme files.
 		 * @param variant $js - String or array(string)
@@ -198,7 +181,7 @@ class Theme
 		{
 			self::set_asset($js, 'js');
 		}
-
+		
 		/**
 		 * Get a list of CSS files
 		 * @return array(string)
@@ -210,10 +193,10 @@ class Theme
 				$assets = self::get_asset('css');
 				return count($assets) ? implode(',', $assets) : '';
 			}
-
+			
 			return self::get_asset('css');
 		}
-
+		
 		/**
 		 * Get a list of JS files
 		 * @return array(string)
@@ -225,10 +208,10 @@ class Theme
 				$assets = self::get_asset('js');
 				return count($assets) ?  implode(',', $assets) : '';
 			}
-
+			
 			return self::get_asset('js');
 		}
-
+				
 		/**
 		 * Set metatags.
 		 * @param array $element	-	Should be array( [name|property|http-equiv] => '', content => '' )
@@ -237,12 +220,12 @@ class Theme
 		public static function set_metatag( $element )
 		{
 			$ci =& get_instance();
-
+			
 			if ( is_array($element) )
 			{
 				krsort($element);
 				$key = '';
-
+				
 				foreach ($element as $pk => $pv)
 				{
 					$pk  = preg_replace(array('/\s/','/\:/','/\_/','/\./'), '-', $pk);
@@ -250,12 +233,12 @@ class Theme
 					$key = strtolower("{$pk}-{$pv}");
 					break;
 				}
-
+				
 				$ci->theme->metatags[ $key ] = $element;
 			}
 
 		} // set_metatag
-
+		
 		/**
 		 * Retrieve the HTML portion with all metatags set.
 		 * @return string
@@ -263,27 +246,25 @@ class Theme
 		public static function metatags()
 		{
 			$ci =& get_instance();
-
+			
             $html = '';
-			$ignore = array('name-title', 'name-charset');   // what will be ignored in main loop
-
-			// add charset (as first meta-tag inside head) to prevent charset problems
-			$html .= PHP_EOL . "\t" . "<meta charset=\"{$ci->theme->metatags['name-charset']['charset']}\" >" . PHP_EOL;
-
-			// add title (the second meta inside head)
+			$ignore = array('name-title');   // what will be ignored in main loop
+            
+			// add title (the first meta inside head)
+            // further add a tab before the tag
 			$html .= PHP_EOL . "\t" . "<title >{$ci->theme->metatags['name-title']['content']}</title>" . PHP_EOL;
-
+			
 			foreach ($ci->theme->metatags as $mk => $mt)
 			{
 				// skip title, coz it uses a specific tag
 				if ( in_array( $mk, $ignore ) ) continue;
-
+				
 				// skip metatags with empty content
 				if ( !isset($mt['content']) || empty($mt['content']) ) continue;
-
+				
                 // create the tag, adding a tab in the begining of line.
 				$html .= "\t" . '<meta ';
-
+				
 				foreach ($mt as $k => $v)
 				{
 					$html .= "{$k}=\"{$v}\" ";
@@ -291,11 +272,11 @@ class Theme
 
 				$html .= '>'.PHP_EOL;
 			}
-
+			
 			return $html . PHP_EOL;
-
+			
 		} // metatags
-
+		
 		/**
 		 * Set the metatag title.
 		 * @param string $str
@@ -305,9 +286,9 @@ class Theme
 		{
 			self::set_metatag( array('name' => 'title', 'content' => $str) );
 			self::set_opengraph('title', $str);	// improve eficiency setting the opengraph tag
-
-		} // set_title
-
+			
+		} // set_title 
+		
 		/**
 		 * Set the metatag description.
 		 * @param string $str
@@ -317,9 +298,9 @@ class Theme
 		{
 			self::set_metatag( array('name' => 'description', 'content' => $str) );
 			self::set_opengraph('description', $str);	// improve eficiency setting the opengraph tag
-
+			
 		} // set_description
-
+		
 		/**
 		 * Set the metatag keywords.
 		 * @param variant (string/array) $str
@@ -331,11 +312,11 @@ class Theme
 			{
 				$piece = implode(',', $piece);
 			}
-
+			
 			self::set_metatag( array('name' => 'keywords', 'content' => $piece) );
-
-		} // set_keywords
-
+			
+		} // set_keywords 
+		
 		/**
 		 * Set the metatag author.
 		 * @param string $str
@@ -344,9 +325,9 @@ class Theme
 		public static function set_author( $str )
 		{
 			self::set_metatag( array('name' => 'author', 'content' => $str) );
-
+			
 		} // set_author
-
+		
 		/**
 		 * Set the metatag publisher.
 		 * @param string $str
@@ -355,9 +336,9 @@ class Theme
 		public static function set_publisher( $str )
 		{
 			self::set_metatag( array('name' => 'publisher', 'content' => $str) );
-
+			
 		} // set_publisher
-
+		
 		/**
 		 * Set the metatag generator.
 		 * @param string $str
@@ -366,9 +347,9 @@ class Theme
 		public static function set_generator( $str )
 		{
 			self::set_metatag( array('name' => 'generator', 'content' => $str) );
-
+			
 		} // set_generator
-
+		
 		/**
 		 * Set the metatag robots.
 		 * @param variant (string/array) $str
@@ -380,11 +361,11 @@ class Theme
 			{
 				$piece = implode(',', $piece);
 			}
-
+			
 			self::set_metatag( array('name' => 'robots', 'content' => $piece) );
-
-		} // set_robots
-
+			
+		} // set_robots 
+		
 		/**
 		 * Set metatags for Open Graph.
 		 * @param string $property
@@ -395,9 +376,9 @@ class Theme
 		{
 			$property = preg_replace('/og\:/', '', $property);
 			self::set_metatag( array('property' => "og:{$property}", 'content' => $content) );
-
-		} // set_opengraph
-
+			
+		} // set_opengraph 
+		
 		/**
 		 * Set metatags for Facebook Open Graph.
 		 * @param string $property
@@ -408,9 +389,9 @@ class Theme
 		{
 			$property = preg_replace('/fb\:/', '', $property);
 			self::set_metatag( array('property' => "fb:{$property}", 'content' => $content) );
-
-		} // set_fb_opengraph
-
+			
+		} // set_fb_opengraph 
+		
 } // Theme
 
 /* End of file Theme.php */
--- themes/mytheme.php
+++ themes/mytheme.php
@@ -2,51 +2,51 @@
 
 <!--
  * CI-Theme
- * Provides a simple way to implement a theme based website or app using Codeigniter.
+ * Provides a simple way to implement a theme based website or app using Codeigniter.  
  *
  * Licensed under MIT license:
  * http://www.opensource.org/licenses/mit-license.php
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
  * to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
  * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @requires PHP5+
  * @author Rogério Taques (rogerio.taques@gmail.com)
  * @see https://github.com/rogeriotaques/ci-theme
- * @version 1.3
- *
+ * @version 1.0
+ * 
 -->
 
 <html>
 
     <head>
-
+        
         <title >Sample Theme for CI-Theme</title>
         <link href="images/favicon.ico" rel="icon" type="image/x-icon" >
 
     </head>
-
+    
     <body >
-
-        <?php
+    
+        <?php 
 
         # here goes the content ...
-        echo $outputs;
+        echo $outputs; 
 
         ?>
-
+            
     </body>
-
+    
 </html>
